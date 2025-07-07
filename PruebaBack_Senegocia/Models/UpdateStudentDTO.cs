6b833282580a296932be65ea1a82da3274f38ce4
Benja <benja.contreras732@gmail.com>
2025-07-06 20:13:00 -0400

    fix

--- PruebaBack_Senegocia/Models/UpdateStudentDTO.cs
+++ PruebaBack_Senegocia/Models/UpdateStudentDTO.cs
@@ -3,6 +3,6 @@
     public class UpdateStudentDTO
     {
         public required string Name { get; set; }
-        public string Email { get; set; } = string.Empty;
+        public string Email { get; set; }
     }
 }
